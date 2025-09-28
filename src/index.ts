8163b2535809dc9cf2470740c512af73849b03ec
Reinvy <reinvy.dev@gmail.com>
2025-09-28 13:28:58 +0700

    feat: Add OpenAPI documentation support
    Integrates the @elysiajs/openapi plugin to automatically generate OpenAPI documentation for the API.
    
    This change also includes updates to the lock file and package.json to reflect the addition of new dependencies required for OpenAPI support, such as `@elysiajs/openapi`, `@types/uuid`, `bcryptjs`, `joi`, `jsonwebtoken`, `uuid`, and `zod`.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Elysia } from "elysia";
+import { openapi } from "@elysiajs/openapi";
+
 import { authRoute } from "./modules/auth/auth.routes.js";
 import { rootRoute } from "./root.js";
 
-const app = new Elysia().use(rootRoute).use(authRoute).listen(3000);
+const app = new Elysia()
+  .use(openapi())
+  .use(rootRoute)
+  .use(authRoute)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
